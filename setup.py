fa1468f36db03801e8da6b6101ea27f29fa9c268
wumao <kanhebei@dingtalk.com.cn>
2020-02-03 17:24:37 +0800

    flask-tinymce

--- setup.py
+++ setup.py
@@ -1,23 +1,43 @@
-# @Time    : 2020/2/2 16:04
-# @Author  : wumao
-# @Email   : kanhebei@dingtalk.com
+"""
+    Flask-TinyMce
+    @Time    : 2020/2/1 11:29
+    @Author  : wumao
+    @Email   : kanhebei@dingtalk.com
 
+    pip install Flask_TinyMce
+
+    from flask_tinymce import TinyMce
+
+
+    tiny_mce = Tinymce(app)
+
+    or
+
+    tiny_mce = Tinymce()
+    tiny_mce.init_app(app)
+
+
+
+"""
 from setuptools import setup
+
 
 setup(
     name='Flask-TinyMce',
-    version='1.0',
-    url='http://example.com/flask-sqlite3/',
+    version='0.1.4',
+    url='https://github.com/kanhebei/flask-tinymce',
     license='MIT',
     author='WuMao',
     author_email='kanhebei@dingtalk.com',
     description='flask tinymce',
     long_description=__doc__,
+    packages=['flask_tinymce'],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
     install_requires=[
-        'Flask'
+        'Flask',
+        'WTForms'
     ],
     classifiers=[
         'Environment :: Web Environment',
